84bad849393c864d256165648644d8c59700f252
iman-adibnazari <iman.adibnazari@gmail.com>
2024-01-28 19:37:50 -0800

    Update rcESCTest.ino

--- src/arduino/rcESCTest/rcESCTest.ino
+++ src/arduino/rcESCTest/rcESCTest.ino
@@ -1,6 +1,8 @@
 #include <Servo.h>
 
-Servo ESC;     // create servo object to control the ESC
+Servo ESC0;     // create servo object to control the ESC
+Servo ESC1;     // create servo object to control the ESC
+Servo ESC2;     // create servo object to control the ESC
 
 int potValue;  // value from the analog pin
 
@@ -10,30 +12,48 @@ float pi = 3.14159;
 float time = 0; // internal time for generating sinusoids
 
 int pump0Pwm = 0;  // PWM signal for pwmp
+int pump1Pwm = 0;  // PWM signal for pwmp
+int pump2Pwm = 0;  // PWM signal for pwmp
 
 float amp0 = 1000; // amplitude of sinusoid (1000 is max, 0 is min)
 float freq0 = 1; // frequency of sinusoid in Hz
 float phi0 = 0;  // phase of sinusoid (in degrees)
 
+float amp1 = 1000; // amplitude of sinusoid (1000 is max, 0 is min)
+float freq1 = 1; // frequency of sinusoid in Hz
+float phi1 = 0;  // phase of sinusoid (in degrees)
 
-void initESC(){
+float amp2 = 1000; // amplitude of sinusoid (1000 is max, 0 is min)
+float freq2 = 1; // frequency of sinusoid in Hz
+float phi2 = 0;  // phase of sinusoid (in degrees)
+
+
+void initESCs(){
 /*
 Initializes ESC to start running
 */
- ESC.write(180);
+ ESC0.write(180);
+ ESC1.write(180);
+ ESC2.write(180);
  Serial.print("Writing High");
-  delay(2000);
+  delay(1000);
 
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    ESC.write(pos);              // tell servo to go to position in variable 'pos'
+    ESC0.write(pos);              // tell servo to go to position in variable 'pos'
+    ESC1.write(pos);              // tell servo to go to position in variable 'pos'
+    ESC2.write(pos);              // tell servo to go to position in variable 'pos'
     Serial.println(pos);
-    delay(15);                       // waits 15 ms for the servo to reach the position
+    delay(5);                       // waits 15 ms for the servo to reach the position
   }
+
+  delay(2000);
 
 }
 void setup() {
   // Attach the ESC on pin 9
-  ESC.attach(9,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+  ESC0.attach(9,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+  ESC1.attach(6,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
+  ESC2.attach(5,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
   Serial.begin(9600);
 
   initESC();
@@ -46,13 +66,19 @@ void loop() {
   time = millis()/1000.0;
 // Compute value of pwm from shifted sin waves
   pump0Pwm = int(amp0*sin(2*pi*freq0*time + phi0*pi/180.0));
+  pump1Pwm = int(amp1*sin(2*pi*freq1*time + phi1*pi/180.0));
+  pump2Pwm = int(amp2*sin(2*pi*freq2*time + phi2*pi/180.0));
 
 // Remap PWM signal for servo library
 pump0Pwm = int(map(pump0Pwm,-1000,1000,0,180));
+pump1Pwm = int(map(pump1Pwm,-1000,1000,0,180));
+pump2Pwm = int(map(pump2Pwm,-1000,1000,0,180));
 Serial.println(pump0Pwm);
 
 // Write PWM signal
-ESC.write(pump0Pwm);
+ESC0.write(pump0Pwm);
+ESC1.write(pump1Pwm);
+ESC2.write(pump2Pwm);
 
 delay(1);
 
